c821b496a64879bf8a1111aead8e048bea3f0d3c
LooOnGit <ironman242k2@gmail.com>
2024-08-27 19:45:33 +0700

    deep sleep mode

--- esp_sim/esp_sim.ino
+++ esp_sim/esp_sim.ino
@@ -7,6 +7,43 @@
 #include <ArduinoJson.h>
 #include <string.h>
 
+#include "soc/rtc_cntl_reg.h"
+#include "soc/rtc.h"
+#include "driver/rtc_io.h"
+
+//=========================================================================Sleep mode==================================================================================
+#define BUTTON_PIN_BITMASK 0x4 // 2^2 in hex
+#define uS_TO_S_FACTOR 1000000ULL  // biến chuyển từ micro giây sang giây
+#define TIME_TO_SLEEP  60     //Thời gian thức dậy
+#define BUTTON_PIN_BITMASK 0x200000000 
+
+
+void print_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason;
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+  switch(wakeup_reason)
+  {
+    case ESP_SLEEP_WAKEUP_EXT0 : 
+      Serial.println("Wakeup caused by external signal using RTC_IO"); 
+      break;
+    case ESP_SLEEP_WAKEUP_EXT1 : 
+      Serial.println("Wakeup caused by external signal using RTC_CNTL"); 
+      break;
+    case ESP_SLEEP_WAKEUP_TIMER : 
+      Serial.println("Wakeup caused by timer"); 
+      break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : 
+      Serial.println("Wakeup caused by touchpad"); 
+      break;
+    case ESP_SLEEP_WAKEUP_ULP : 
+      Serial.println("Wakeup caused by ULP program"); 
+      break;
+    default : 
+      Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); 
+      break;
+  }
+}
+
 //----------------------------------------------------------Manager ID-------------------------------------------------------------------------------------------
 #define ID "GW9783"
 typedef struct id{
@@ -24,20 +61,24 @@ id managerID;
 #define GSM_SERIAL          1
 #define GSM_RX              16      // ESP32 TX - SIM RX
 #define GSM_TX              17      // ESP32 RX - SIM TX
-#define GSM_POWER           15      // GSM Power Enable
+#define GSM_POWER           32      // GSM Power Enable
 #define GSM_BR              115200
 
 //--------------------------------------------------------------LoRa sx1278 Ra-02------------------------------------------------------------------------
 #define ss 5
 #define rst 14
 #define dio0 2
-
 //-------------------------------------------------------------JSON---------------------------------------------------------------------------------
 // DynamicJsonDocument managerID(1024);
 // int numberPartners = 0;
 JsonDocument dataFull;
 JsonDocument recData;
 
+void IRAM_ATTR interrupt() {
+  uint64_t GPIO_reason = esp_sleep_get_ext1_wakeup_status();
+  Serial.print("GPIO that triggered the wake up: GPIO ");
+  Serial.println((log(GPIO_reason)) / log(2), 0);
+}
 //================================================================Time=====================================================================================================
 int interval = 5000;          // interval between sends
 long lastSendTime = 0;        // time of last packet send
@@ -55,17 +96,15 @@ String buffer = "";
 char *data = (char *) malloc(1024); 
 bool atMode = true;
 
-const char* publishTopic ="main";
+const char* publishTopic = "channels/2606003/publish";
 const unsigned long postingInterval = 20L * 1000L;
 unsigned long lastUploadedTime = 0;
 
 PPPOSClient ppposClient;
 PubSubClient client(ppposClient);
-// char* server = "mqtt3.thingspeak.com";
-char* server = "127.0.0.1";
+char* server = "mqtt3.thingspeak.com";
 //----------------------------------------------------------------function sim------------------------------------------------------------------------
-bool sendCommandWithAnswer(String cmd, String ans)
-{
+bool sendCommandWithAnswer(String cmd, String ans){
    PPPOS_write((char *)cmd.c_str());
    unsigned long _tg = millis();
    while(true){
@@ -88,8 +127,7 @@ bool sendCommandWithAnswer(String cmd, String ans)
    return false;
 }
 
-int8_t  AT_CheckCSQ(void)
-{
+int8_t  AT_CheckCSQ(void){
    int csq = 0;
    PPPOS_write("AT+CSQ\r\n");
    unsigned long _tg = millis();
@@ -118,8 +156,7 @@ int8_t  AT_CheckCSQ(void)
    return csq;
 }
 
-bool startPPPOS()
-{  
+bool startPPPOS(){  
   if (!sendCommandWithAnswer("ATD*99***1#\r\n", "CONNECT 115200")) { return false; }
   atMode = false;
   PPPOS_start(); 
@@ -144,11 +181,10 @@ void SIM_connect_PPP(void)
   {
       SIM_reset();
       bool SyncOK = false;
-
       Serial.println("SIM Sync"); 
       for(int i = 0; i < 100; i++)
       {
-        delay(500);
+        delay(1000);
         Serial.print("."); 
         if(!sendCommandWithAnswer("AT\r\n", "OK")){
           continue;
@@ -159,7 +195,7 @@ void SIM_connect_PPP(void)
         SyncOK = true;
         break;
       }
-      
+
       Serial.println("Check Signal Quality"); 
       for(int i = 0; i < 50; i++){
         if(AT_CheckCSQ() > 0){
@@ -187,8 +223,7 @@ void SIM_connect_PPP(void)
   }
 }
 
-void callback(char* topic, byte* payload, unsigned int length) 
-{
+void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
@@ -198,30 +233,20 @@ void callback(char* topic, byte* payload, unsigned int length)
   Serial.println();
 }
 
-//======================================= publising as string
-void publishMessage(const char* topic, String payload , boolean retained)
-{
-  if (client.publish(topic, payload.c_str()))
-      Serial.println("Message publised ["+String(topic)+"]: "+payload);
+void publishMessage(const char* topic, String payload, boolean retained){
+  if (client.publish(topic, payload.c_str(), retained))
+    Serial.println("Message published ["+String(topic)+"]: "+payload);
 }
 
-void reconnect() 
-{
-  // Loop until we're reconnected
+void reconnect() {
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    if (client.connect("JxUNHxwxFgYBEjkBFBUqGi4", "JxUNHxwxFgYBEjkBFBUqGi4","4Fusq7vyk/SWXKS19EGQLRWQ")) {
+    if (client.connect("OjcNEyo1Iy0xFTApIzoVBgg", "OjcNEyo1Iy0xFTApIzoVBgg", "qDYTsURL6GZrknqpBlPRkiU6")) {
       Serial.println("connected");
-      // Once connected, publish an announcement...
-      // client.publish("outTopic135","hello world from module sim");
-      // ... and resubscribe
-      // client.subscribe("inTopic");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
       delay(5000);
     }
   }
@@ -232,6 +257,9 @@ void merge(JsonObject& dataFull, JsonObjectConst& dataRec)
 {
    for (JsonPairConst kvp : dataRec)
    {
+    if (strcmp(kvp.key().c_str(), "ID") == 0) {
+      continue;
+    }
      dataFull[kvp.key()] = kvp.value();
    }
 }
@@ -268,18 +296,17 @@ void onReceive(int packetSize)
   JsonObject object1 = dataFull.as<JsonObject>();
   merge(object1, object2);
   const char *recID = recData["ID"];
+  
   Serial.println("\nReceived from: " + String(recID));
-  Serial.println("Message length: " + String(Incoming.length()));
-  Serial.println("RSSI: " + String(LoRa.packetRssi()));
-  Serial.println("data FULL: ");
-  serializeJson(dataFull, Serial);
+  // Serial.println("Message length: " + String(Incoming.length()));
+  // Serial.println("RSSI: " + String(LoRa.packetRssi()));
   //---------------------------------------- 
 }
 
 void receivedFromNodes()
 {
   int times = 0;
-  for(int element = 0; element < managerID.numberID; element++){
+  for(int element = 1; element < managerID.numberID; element++){
     while(times < 5){
       if (millis() - lastSendTime > interval) {
         sendMessage(managerID.idJson[element]);
@@ -289,13 +316,14 @@ void receivedFromNodes()
         lastSendTime = millis();
       }
       onReceive(LoRa.parsePacket());
-      if(dataFull["ID"] == managerID.idJson[element]){
-        deserializeJson(dataFull, "{}");//clear Json
+      if(recData["ID"] == managerID.idJson[element]){
         break;
       }
     }
     times = 0;
   }
+  Serial.println("data FULL: ");
+  serializeJson(dataFull, Serial);
 }
 
 //-------------------------------------------------------------------------------MANAGE ID-------------------------------------------------------------------
@@ -339,14 +367,27 @@ void readIdfromRom()
   Serial.println("All ID: ");
   serializeJson(managerID.idJson, Serial);
 }
-
 //--------------------------------------------------------------------------------setup---------------------------------------------------------------------
 void setup()
 {
+  
   managerID.addr = 0;
-  dataFull["fd"] = 0;
+  dataFull["MyID"] = ID;
   managerID.numberID = 0;
+  pinMode(GSM_POWER, OUTPUT);
+  gpio_hold_dis((gpio_num_t)GSM_POWER);
   Serial.begin(115200);
+  Serial2.begin(115200);
+  digitalWrite(GSM_POWER, LOW);
+  delaySIM(500);
+  digitalWrite(GSM_POWER, HIGH);
+  delaySIM(10000);
+  Serial2.println("AT+CLBS=4");
+  delaySIM(500);
+  while (Serial2.available())
+  {
+    Serial.write(Serial2.read());//Forward what Software Serial received to Serial Port
+  }
   char *id = ID;
   //============================================================================My Device ID================================================================
   Serial.println("My ID: " + String(ID));
@@ -359,15 +400,14 @@ void setup()
   //============================================================================Write MyID to EPPROM================================================================
   writeIdToRom(id);
   writeIdToRom("ND7969");
+  writeIdToRom("ND7970");
   //============================================================================Init SIM======================================================================
   /*  Init PPP  */
-  // PPPOS_init(GSM_TX, GSM_RX, GSM_BR, GSM_SERIAL, ppp_user, ppp_pass);
-  // pinMode(GSM_POWER, OUTPUT);
+  PPPOS_init(GSM_TX, GSM_RX, GSM_BR, GSM_SERIAL, ppp_user, ppp_pass);
 
-  // // SIM_connect_PPP();
-
-  // // client.setServer(server, 1883);
-  // // client.setCallback(callback);
+  SIM_connect_PPP();
+  client.setServer(server, 1883);
+  client.setCallback(callback);
 
   //=============================================================================Init Lora====================================================================
   LoRa.setPins(ss, rst, dio0);
@@ -383,35 +423,85 @@ void setup()
   LoRa.setSyncWord(0x12);
   Serial.println("LoRa init succeeded.");
   readIdfromRom();
+
+  // pinMode(dio1, INPUT_PULLDOWN);
+  // esp_sleep_enable_ext1_wakeup(0x400000000, ESP_EXT1_WAKEUP_ANY_HIGH);
+  // attachInterrupt(digitalPinToInterrupt(dio1), interrupt, RISING);
+
+
+  // in ra nguồn đánh thức esp32
+  // print_wakeup_reason();
+
+
+  // gọi hàm thức dậy mỗi 5s
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) +
+  " Seconds");
 }
 
 void loop()
 {
+
+  Serial2.println("AT+CLK?");
+  delay(500);
+  while (Serial2.available())
+  {
+    Serial.write(Serial2.read());//Forward what Software Serial received to Serial Port
+  }
   receivedFromNodes();
-  // if (!PPPOS_isConnected() || atMode){
-  //   data = PPPOS_read();
-  //   if (data != NULL){
-  //     Serial.println(data);  
-  //   }
-  // }
 
-  // if(PPPOS_isConnected())
-  // {
-  //   if (!client.connected()) {
-  //     reconnect();
-  //   }
-  //   client.loop();
-  //   if (millis() - lastUploadedTime > postingInterval) 
-  //   { // The uploading interval must be > 15 seconds 
-  //     int sensorValue_1 = random(100); // replace with your sensor value
-  //     int sensorValue_2=random(100);  // replace with your sensor value
-  //     //int sensorValue_3=random(100);  // if you want to use three sensors enable this line
-      
-  //     String dataText = String("field1=" + String(sensorValue_1)+ "&field2=" + String(sensorValue_2)+"&status=MQTTPUBLISH");
-  //     //String dataText = String("field1=" + String(sensorValue_1)+ "&field2=" + String(sensorValue_2)+"&field3=" + String(sensorValue_3)); // example for publish tree sensors
-  //     publishMessage(publishTopic,dataText,true);    
-  //     lastUploadedTime = millis();
-  //   }    
-  // }
+  if (!PPPOS_isConnected() || atMode){
+    data = PPPOS_read();
+    if (data != NULL){
+      Serial.println(data);  
+    }
+  }
 
+  if (!PPPOS_isConnected() || atMode){
+      data = PPPOS_read();
+      if (data != NULL){
+        Serial.println(data);  
+      }
+    }
+
+    if(PPPOS_isConnected()) {
+      if (!client.connected()) {
+        reconnect();
+      }
+      client.loop();
+      if (millis() - lastUploadedTime > postingInterval) {
+        const char* temp = dataFull["TEMP"];
+        const char* hum = dataFull["HUM"];
+        const char* sal = dataFull["SAL"];
+        const char* q = dataFull["Q"];
+
+        String dataText = String("field1=") + temp + "&field2=" + hum + "&field3=" + sal + "&field4=" + q + "&status=MQTTPUBLISH";
+        publishMessage(publishTopic, dataText, true);    
+        lastUploadedTime = millis();
+    }    
+  }
+  deserializeJson(recData, "{}");//clear Json
+  deserializeJson(dataFull, "{}");//clear Json
+  dataFull["MyID"] = ID;
+  delay(1000);
+  digitalWrite(GSM_POWER, LOW);
+  gpio_hold_en((gpio_num_t)GSM_POWER);
+  esp_deep_sleep_start();
+} 
+
+void test_sim800_module()
+{
+  Serial2.println("AT+CLBS=4");
+  delay(500);
+  while (Serial2.available())
+  {
+    Serial.write(Serial2.read());//Forward what Software Serial received to Serial Port
+  }
+}
+
+void delaySIM(int delay){
+  int firstTime = millis();
+  while((millis() - firstTime) > delay){
+
+  }
 }
